976a9f73e7325d968a3450d037dd15b490c062d1
Illusion137 <raygoza.d@hotmail.com>
2026-03-31 00:56:54 -0700

    Illusi 20.0.2 + fix bug because RNTPvE wasn't in cache for some reason... + link toast bug fix

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default (config: ConfigContext['config']): ExpoConfig => ({
     ...config,
     "name": IS_DEV ? 'Illusi (Dev)' : 'Illusi',
     "slug": "Illusi",
-    "version": "20.0.1",
+    "version": "20.0.2",
     // "orientation": "portrait",
     // "icon": "./assets/icon.png",
     // "userInterfaceStyle": "dark",
